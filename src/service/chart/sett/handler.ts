5ec199012aa180bd677d3aea45eb424acf0b80e4
Cristian Vallejo <cdv1013@gmail.com>
2021-01-30 14:22:00 -0500

    address inconsistency

--- src/service/chart/sett/handler.ts
+++ src/service/chart/sett/handler.ts
@@ -2,16 +2,12 @@ import { ASSET_DATA } from '../../util/constants';
 import { EventInput, getAssetData, respond } from '../../util/util';
 
 export const handler = async (event: EventInput) => {
-	if (event.source === 'serverless-plugin-warmup') {
-		return respond(200);
-	}
-
 	const asset = event.pathParameters!.settName;
+	if (!asset) return respond(400, {statusCode: 400, message: 'settName is a required path parameter'});
 	const count = event.queryStringParameters ? Number(event.queryStringParameters.count) : null;
 	const data = await getAssetData(ASSET_DATA, asset, count);
 	if (!data) return respond(500);
 
 	const points = data.map((item) => ({ x: item.timestamp, y: item.value }));
-
 	return respond(200, points);
 };
--- src/service/chart/sett/performance/handler.ts
+++ src/service/chart/sett/performance/handler.ts
@@ -2,18 +2,13 @@ import { ASSET_DATA } from '../../../util/constants';
 import { EventInput, getAssetData, respond } from '../../../util/util';
 
 export const handler = async (event: EventInput) => {
-	if (event.source === 'serverless-plugin-warmup') {
-		return 200;
-	}
-
-	const asset = event.pathParameters ? event.pathParameters.settName : null;
+	const asset = event.pathParameters!.settName;
+	if (!asset) return respond(400, {statusCode: 400, message: 'settName is a required path parameter'});
 	const count = event.queryStringParameters ? Number(event.queryStringParameters.count) : null;
-
 	const data = await getAssetData(ASSET_DATA, asset, count);
 	if (!data) return respond(500);
 
 	const initialRatioDiff = data.length > 0 ? data[0].ratio - 1 : 0;
-
 	const points = data.map((item) => ({ x: item.timestamp, y: item.ratio - initialRatioDiff }));
 	return respond(200, points);
 };
