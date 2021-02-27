796fed0066233672cbb0c3375628be37ddcfde34
Kaneki <ethkaneki@protonmail.com>
2021-02-27 00:57:51 -0500

    feat(badger-api): Add graphql-codegen and generate protocol schemas

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
 		'.*',
 		'build',
 		'node_modules',
+		'src/graphql/generated',
 	],
 	parserOptions: {
 		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
