41e20a39889e3f4e3f1ef3658a4bb5ee5e43848b
jintao <hellojintao@protonmail.com>
2021-03-18 14:34:33 -0700

    feat: multi-chain support for v2 api

---
+++ src/harvests/HarvestsService.ts
@@ -0,0 +1,23 @@
+import { Service } from '@tsed/common';
+import { GraphQLClient } from 'graphql-request';
+import { BADGER_DAO_URL } from '../config/constants';
+import {
+  getSdk,
+  HarvestsQuery,
+  HarvestsQueryVariables,
+  Sdk as BadgerDaoGraphqlSdk,
+} from '../graphql/generated/badger-dao';
+
+@Service()
+export class HarvestsService {
+  private badgerDaoGraphqlSdk: BadgerDaoGraphqlSdk;
+
+  constructor() {
+    const badgerDaoGraphqlClient = new GraphQLClient(BADGER_DAO_URL);
+    this.badgerDaoGraphqlSdk = getSdk(badgerDaoGraphqlClient);
+  }
+
+  async listHarvests(options: HarvestsQueryVariables): Promise<HarvestsQuery> {
+    return this.badgerDaoGraphqlSdk.Harvests(options);
+  }
+}
