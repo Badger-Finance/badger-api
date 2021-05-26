809dc2616724db658e0c6a39c8ef54d87bb8d6b4
Kaneki <79668923+ethkaneki@users.noreply.github.com>
2021-05-26 10:26:21 -0400

    fix(api): Fetch live sett in lp token indexer (#162)

--- src/indexer/liquidity-pool-token-balances-indexer.ts
+++ src/indexer/liquidity-pool-token-balances-indexer.ts
@@ -1,44 +1,49 @@
 import { getDataMapper } from '../aws/dynamodb.utils';
-import { bscSetts } from '../chains/config/bsc.config';
-import { ethSetts } from '../chains/config/eth.config';
+import { loadChains } from '../chains/chain';
 import { UniV2PairFragment } from '../graphql/generated/uniswap';
 import { getPrice, inCurrency } from '../prices/prices.utils';
-import { getCachedSett } from '../setts/setts.utils';
 import { CachedTokenBalance } from '../tokens/interfaces/cached-token-balance.interface';
 import { TokenPrice } from '../tokens/interfaces/token-price.interface';
 import { TokensService } from '../tokens/tokens.service';
-import { tokenBalancesToCachedLiquidityPoolTokenBalance } from './indexer.utils';
+import { settToCachedSnapshot, tokenBalancesToCachedLiquidityPoolTokenBalance } from './indexer.utils';
 
 export async function refreshLpTokenBalances() {
-  const settDefinitions = [...bscSetts, ...ethSetts];
+  const chains = loadChains();
 
-  for (const settDefinition of settDefinitions) {
-    const poolData = await TokensService.getPoolData(settDefinition);
-    // Silently skip any missing data
-    if (!poolData) {
-      continue;
-    }
-
-    const mapper = getDataMapper();
-    let cachedLiquidityPoolTokenBalance;
-    try {
-      const pairId = settDefinition.depositToken.toLowerCase();
-      const sett = await getCachedSett(settDefinition);
-      const { balance } = sett;
-      const { protocol } = settDefinition;
-      const { pair } = poolData;
-      if (!pair || !protocol) {
+  for (const chain of chains) {
+    const settDefinitions = chain.setts;
+    for (const settDefinition of settDefinitions) {
+      const poolData = await TokensService.getPoolData(settDefinition);
+      // Silently skip any missing data
+      if (!poolData) {
         continue;
       }
 
-      cachedLiquidityPoolTokenBalance = tokenBalancesToCachedLiquidityPoolTokenBalance(
-        pairId,
-        protocol,
-        await pairToCachedTokenBalance(pair, balance),
-      );
-      await mapper.put(cachedLiquidityPoolTokenBalance);
-    } catch (err) {
-      console.log({ message: err.message, cachedLiquidityPoolTokenBalance });
+      const mapper = getDataMapper();
+      let cachedLiquidityPoolTokenBalance;
+      try {
+        const pairId = settDefinition.depositToken.toLowerCase();
+        const settSnapshot = await settToCachedSnapshot(chain, settDefinition);
+        if (!settSnapshot) {
+          continue;
+        }
+
+        const { balance } = settSnapshot;
+        const { protocol } = settDefinition;
+        const { pair } = poolData;
+        if (!pair || !protocol) {
+          continue;
+        }
+
+        cachedLiquidityPoolTokenBalance = tokenBalancesToCachedLiquidityPoolTokenBalance(
+          pairId,
+          protocol,
+          await pairToCachedTokenBalance(pair, balance),
+        );
+        await mapper.put(cachedLiquidityPoolTokenBalance);
+      } catch (err) {
+        console.log({ message: err.message, cachedLiquidityPoolTokenBalance });
+      }
     }
   }
 }
