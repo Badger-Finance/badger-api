1a8d0c5175edc989789990b1a763c19228d4e41b
Kaneki <79668923+ethkaneki@users.noreply.github.com>
2021-06-05 18:00:20 -0400

    feat(api): Add time range and granularity arguments to ChartsQueryDto (#193)

--- src/charts/charts.controller.spec.ts
+++ src/charts/charts.controller.spec.ts
@@ -2,6 +2,7 @@ import { PlatformTest } from '@tsed/common';
 import { NotFound, UnprocessableEntity } from '@tsed/exceptions';
 import SuperTest from 'supertest';
 import { Server } from '../Server';
+import { ChartGranularity } from './enums/chart-granularity.enum';
 
 describe('ChartsController', () => {
   let request: SuperTest.SuperTest<SuperTest.Test>;
@@ -24,10 +25,146 @@ describe('ChartsController', () => {
         });
       });
 
+      describe('with an invalid address', () => {
+        it('returns a 422', async (done) => {
+          const { body } = await request
+            .get('/v2/charts')
+            .query({
+              id: 'bc1qa5wkgaew2dkv56kfvj49j0av5nml45x9ek9hz6',
+            })
+            .expect(UnprocessableEntity.STATUS);
+
+          expect(body).toMatchSnapshot();
+          done();
+        });
+      });
+
       describe('with an invalid chain', () => {
         it('returns a 422', async (done) => {
           const { body } = await request
-            .get('/v2/charts?id=0x4206942069420694206942069420694206942069&chain=safemoonchain')
+            .get('/v2/charts')
+            .query({
+              id: '0x4206942069420694206942069420694206942069',
+              chain: 'safemoonchain',
+            })
+            .expect(UnprocessableEntity.STATUS);
+
+          expect(body).toMatchSnapshot();
+          done();
+        });
+      });
+
+      describe('with start but no end', () => {
+        it('returns a 422', async (done) => {
+          const { body } = await request
+            .get('/v2/charts')
+            .query({
+              id: '0x4206942069420694206942069420694206942069',
+              start: new Date().toISOString(),
+            })
+            .expect(UnprocessableEntity.STATUS);
+
+          expect(body).toMatchSnapshot();
+          done();
+        });
+      });
+
+      describe('with end but no start', () => {
+        it('returns a 422', async (done) => {
+          const { body } = await request
+            .get('/v2/charts')
+            .query({
+              id: '0x4206942069420694206942069420694206942069',
+              end: new Date().toISOString(),
+            })
+            .expect(UnprocessableEntity.STATUS);
+
+          expect(body).toMatchSnapshot();
+          done();
+        });
+      });
+
+      describe('with an invalid time range', () => {
+        it('returns a 422', async (done) => {
+          const start = new Date().toISOString();
+          const end = new Date(Date.now() - 1).toISOString();
+          const { body } = await request
+            .get('/v2/charts')
+            .query({
+              id: '0x4206942069420694206942069420694206942069',
+              start,
+              end,
+            })
+            .expect(UnprocessableEntity.STATUS);
+
+          expect(body).toMatchSnapshot();
+          done();
+        });
+      });
+
+      describe('with an invalid granularity', () => {
+        it('returns a 422', async (done) => {
+          const { body } = await request
+            .get('/v2/charts')
+            .query({
+              id: '0x4206942069420694206942069420694206942069',
+              granularity: 'second',
+            })
+            .expect(UnprocessableEntity.STATUS);
+
+          expect(body).toMatchSnapshot();
+          done();
+        });
+      });
+
+      describe('with an invalid period', () => {
+        it('returns a 422', async (done) => {
+          const { body } = await request
+            .get('/v2/charts')
+            .query({
+              id: '0x4206942069420694206942069420694206942069',
+              period: -1,
+            })
+            .expect(UnprocessableEntity.STATUS);
+
+          expect(body).toMatchSnapshot();
+          done();
+        });
+      });
+
+      describe('with a large time range for HOUR granularity', () => {
+        it('returns a 422', async (done) => {
+          const start = '2021-05-01T00:00:00.000Z';
+          const end = '2021-06-01T00:00:00.000Z';
+          const { body } = await request
+            .get('/v2/charts')
+            .query({
+              id: '0x4206942069420694206942069420694206942069',
+              start,
+              end,
+              granularity: ChartGranularity.HOUR,
+              period: 1,
+            })
+            .expect(UnprocessableEntity.STATUS);
+
+          expect(body).toMatchSnapshot();
+          done();
+        });
+      });
+
+      describe('with a large time range for DAY granularity', () => {
+        it('returns a 422', async (done) => {
+          const start = '2021-01-01T00:00:00.000Z';
+          const end = '2021-06-01T00:00:00.000Z';
+          const { body } = await request
+            .get('/v2/charts')
+            .query({
+              id: '0x4206942069420694206942069420694206942069',
+              start,
+              end,
+              granularity: ChartGranularity.DAY,
+              period: 1,
+            })
             .expect(UnprocessableEntity.STATUS);
 
           expect(body).toMatchSnapshot();
@@ -36,10 +173,13 @@ describe('ChartsController', () => {
       });
     });
 
-    describe('with an invalid address', () => {
+    describe('with an unsupported address', () => {
       it('returns a 404', async (done) => {
         const { body } = await request
-          .get('/v2/charts?id=0x4206942069420694206942069420694206942069')
+          .get('/v2/charts')
+          .query({
+            id: '0x4206942069420694206942069420694206942069',
+          })
           .expect(NotFound.STATUS);
 
         expect(body).toMatchSnapshot();
--- src/charts/charts.controller.ts
+++ src/charts/charts.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Inject, QueryParams, UsePipe } from '@tsed/common';
-import { NotFound } from '@tsed/exceptions';
+import { NotFound, UnprocessableEntity } from '@tsed/exceptions';
 import { ContentType } from '@tsed/schema';
 import { ethers } from 'ethers';
 import { Chain } from '../chains/config/chain.config';
-import { ValidationPipe } from '../common/validation-pipe';
+import { ValidationPipe } from '../common/decorators/validation-pipe';
 import { SettSnapshot } from '../setts/interfaces/sett-snapshot.interface';
 import { SettsService } from '../setts/setts.service';
 import { getSettSnapshots } from '../setts/setts.utils';
 import { ChartsQueryDto } from './dto/charts-query.dto';
+import { ChartGranularity } from './enums/chart-granularity.enum';
 
 @Controller('/charts')
 export class ChartsController {
@@ -17,13 +18,70 @@ export class ChartsController {
   @Get()
   @ContentType('json')
   async getChart(
-    @UsePipe(ValidationPipe) @QueryParams() { id: settToken, chain }: ChartsQueryDto,
+    @UsePipe(ValidationPipe)
+    @QueryParams()
+    query: ChartsQueryDto,
   ): Promise<SettSnapshot[]> {
+    const now = new Date();
+    const yesterday = new Date();
+    yesterday.setHours(now.getHours() - 24);
+
+    const {
+      id: settToken,
+      chain,
+      start = yesterday,
+      end = now,
+      period = 1,
+      granularity = ChartGranularity.HOUR,
+    } = query;
+
+    const { isValid, error } = this.isValidGranularityPeriod(granularity, period, start, end);
+    if (!isValid && error) {
+      throw new UnprocessableEntity(error);
+    }
+
     const checksumContract = ethers.utils.getAddress(settToken);
     const sett = Chain.getChain(chain).setts.find((sett) => sett.settToken === checksumContract);
     if (!sett) {
       throw new NotFound(`${checksumContract} is not a valid sett`);
     }
     return getSettSnapshots(sett);
+  }
+
+  private isValidGranularityPeriod(
+    granularity: ChartGranularity,
+    period: number,
+    start: Date,
+    end: Date,
+  ): { isValid: boolean; error?: string } {
+    const maxDataPointsToReturn = 50;
+
+    if (start >= end) {
+      return {
+        isValid: false,
+        error: '"start" must be strictly less than "end"',
+      };
+    }
+
+    let returnedDataPoints: number;
+    const diffInMs = new Date(end).getTime() - new Date(start).getTime();
+    if (granularity === ChartGranularity.DAY) {
+      const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+      returnedDataPoints = Math.floor(diffInDays / period);
+    } else {
+      const diffInHours = diffInMs / (1000 * 60 * 60);
+      returnedDataPoints = Math.floor(diffInHours / period);
+    }
+
+    if (returnedDataPoints > maxDataPointsToReturn) {
+      return {
+        isValid: false,
+        error: 'Too many data points to return. Increase the period or shorten the time range.',
+      };
+    }
+
+    return {
+      isValid: true,
+    };
   }
 }
