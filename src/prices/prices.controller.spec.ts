0183466458781534805dd3d7f77619fc5b6d2855
jintao <hellojintao@protonmail.com>
2021-06-08 13:58:27 -0400

    chore: use deterministic snapshot price

--- src/prices/prices.controller.spec.ts
+++ src/prices/prices.controller.spec.ts
@@ -2,7 +2,6 @@ import { PlatformTest } from '@tsed/common';
 import { BadRequest } from '@tsed/exceptions';
 import SuperTest from 'supertest';
 import { Server } from '../Server';
-import { randomValue } from '../test/tests.utils';
 import { bscTokensConfig } from '../tokens/config/bsc-tokens.config';
 import { ethTokensConfig } from '../tokens/config/eth-tokens.config';
 import { PricesService } from './prices.service';
@@ -19,10 +18,12 @@ describe('PricesController', () => {
 
   afterEach(PlatformTest.reset);
 
+  const getPrice = (address: string): number => parseInt(address.slice(0, 3), 16);
+
   describe('GET /v2/prices', () => {
     describe('with no specified chain', () => {
       it('returns eth token config', async (done: jest.DoneCallback) => {
-        const result = Object.fromEntries(Object.keys(ethTokensConfig).map((token) => [token, randomValue()]));
+        const result = Object.fromEntries(Object.keys(ethTokensConfig).map((token) => [token, getPrice(token)]));
         jest.spyOn(pricesService, 'getPriceSummary').mockImplementationOnce(() => Promise.resolve(result));
         const { body } = await request.get('/v2/prices').expect(200);
         expect(body).toMatchSnapshot();
@@ -32,7 +33,7 @@ describe('PricesController', () => {
 
     describe('with a specified chain', () => {
       it('returns the specified token config for eth', async (done: jest.DoneCallback) => {
-        const result = Object.fromEntries(Object.keys(ethTokensConfig).map((token) => [token, randomValue()]));
+        const result = Object.fromEntries(Object.keys(ethTokensConfig).map((token) => [token, getPrice(token)]));
         jest.spyOn(pricesService, 'getPriceSummary').mockImplementationOnce(() => Promise.resolve(result));
         const { body } = await request.get('/v2/prices?chain=eth').expect(200);
         expect(body).toMatchSnapshot();
@@ -40,7 +41,7 @@ describe('PricesController', () => {
       });
 
       it('returns the specified token config for bsc', async (done: jest.DoneCallback) => {
-        const result = Object.fromEntries(Object.keys(bscTokensConfig).map((token) => [token, randomValue()]));
+        const result = Object.fromEntries(Object.keys(bscTokensConfig).map((token) => [token, getPrice(token)]));
         jest.spyOn(pricesService, 'getPriceSummary').mockImplementationOnce(() => Promise.resolve(result));
         const { body } = await request.get('/v2/prices?chain=bsc').expect(200);
         expect(body).toMatchSnapshot();
