ad20668eee6302c8e1e854b475a8160f9f0f5da2
jintao <hellojintao@protonmail.com>
2021-06-11 13:34:43 -0400

    chore: disable long local job, turn down new sett noise, fix db writes

--- src/indexer/sett-indexer.ts
+++ src/indexer/sett-indexer.ts
@@ -2,10 +2,19 @@ import { NotFound } from '@tsed/exceptions';
 import { getDataMapper } from '../aws/dynamodb.utils';
 import { loadChains } from '../chains/chain';
 import { Chain } from '../chains/config/chain.config';
+import { IS_OFFLINE } from '../config/constants';
 import { SettDefinition } from '../setts/interfaces/sett-definition.interface';
 import { getIndexedBlock, settToSnapshot } from './indexer.utils';
 
+/**
+ * Index a sett's historic data via the graph + web3.
+ * This is an expensive process to do so locally always and
+ * as such will be disabled while running offline.
+ */
 export const indexAsset = async (): Promise<void> => {
+  if (IS_OFFLINE) {
+    return;
+  }
   const chains = loadChains();
   await Promise.all(
     chains.flatMap(async (chain) => Promise.all(chain.setts.map(async (sett) => indexSett(chain, sett)))),
