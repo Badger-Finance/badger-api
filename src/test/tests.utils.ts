6f22ba843b1f6758a05ac58617252d62cb5a3779
jintao <hellojintao@protonmail.com>
2021-07-26 12:32:40 -0400

    chore: add accounts utils tests

--- src/test/tests.utils.ts
+++ src/test/tests.utils.ts
@@ -1,5 +1,12 @@
 import { DataMapper, QueryIterator, StringToAnyObjectMap } from '@aws/dynamodb-data-mapper';
 import createMockInstance from 'jest-create-mock-instance';
+import { loadChains } from '../chains/chain';
+import { ONE_DAY_MS, SAMPLE_DAYS } from '../config/constants';
+import { CachedSettSnapshot } from '../setts/interfaces/cached-sett-snapshot.interface';
+import { SettDefinition } from '../setts/interfaces/sett-definition.interface';
+import { SettSnapshot } from '../setts/interfaces/sett-snapshot.interface';
+
+export const TEST_ADDR = '0xe6487033F5C8e2b4726AF54CA1449FEC18Bd1484';
 
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 export const setupMapper = (items: unknown[], filter?: (items: unknown[]) => unknown[]) => {
@@ -20,3 +27,46 @@ export const randomValue = (min?: number, max?: number): number => {
   const maxPrice = max || 50000;
   return minPrice + Math.random() * (maxPrice - minPrice);
 };
+
+export function randomSnapshot(settDefinition?: SettDefinition): CachedSettSnapshot {
+  const sett = settDefinition || randomSett();
+  return Object.assign(new CachedSettSnapshot(), {
+    address: sett.settToken,
+    balance: randomValue(),
+    ratio: 1,
+    settValue: randomValue(),
+    supply: randomValue(),
+    updatedAt: Date.now(),
+  });
+}
+
+export function randomSett(): SettDefinition {
+  const definitions = loadChains().flatMap((chain) => chain.setts);
+  return definitions[Math.floor(Math.random() * definitions.length)];
+}
+
+export function randomSnapshots(settDefinition?: SettDefinition, count?: number): SettSnapshot[] {
+  const snapshots: SettSnapshot[] = [];
+  const snapshotCount = count || SAMPLE_DAYS;
+  const sett = settDefinition || randomSett();
+  for (let i = 0; i < snapshotCount; i++) {
+    snapshots.push(
+      Object.assign(new SettSnapshot(), {
+        asset: sett.name,
+        height: 0,
+        timestamp: Date.now(),
+        balance: randomValue(),
+        supply: randomValue(),
+        ratio: 1,
+        value: randomValue(),
+      }),
+    );
+  }
+  return snapshots;
+}
+
+export function randomPerformance(): [SettSnapshot, SettSnapshot] {
+  const [current, initial] = randomSnapshots(randomSett(), 2);
+  initial.timestamp = current.timestamp - ONE_DAY_MS;
+  return [current, initial];
+}
