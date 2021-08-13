c433b55c0a730db578f3108aed69cfc649a329c2
jintao <hellojintao@protonmail.com>
2021-08-13 00:49:32 -0500

    chore: remove chart data padding

--- src/charts/charts.service.ts
+++ src/charts/charts.service.ts
@@ -1,5 +1,4 @@
 import { Service } from '@tsed/di';
-import { Chain } from '../chains/config/chain.config';
 import { SettDefinition } from '../setts/interfaces/sett-definition.interface';
 import { SettSnapshot } from '../setts/interfaces/sett-snapshot.interface';
 import { getSettSnapshotsInRange } from '../setts/setts.utils';
@@ -8,7 +7,6 @@ import { ChartGranularity } from './enums/chart-granularity.enum';
 @Service()
 export class ChartsService {
   async getChartData(
-    chain: Chain,
     sett: SettDefinition,
     start: Date,
     end: Date,
@@ -21,36 +19,6 @@ export class ChartsService {
     const interval = granularity === ChartGranularity.HOUR ? 2 : 48 * period;
     // filter data down to selected granularity period combination
     snapshots = snapshots.filter((_s, i) => i % interval === 0);
-
-    const expectedDataPoints = this.getRequestedDataPoints(start, end, granularity, period);
-    if (snapshots.length < expectedDataPoints) {
-      let lastSnapshot = snapshots[snapshots.length - 1];
-      if (!lastSnapshot) {
-        lastSnapshot = {
-          address: sett.settToken,
-          value: 0,
-          balance: 0,
-          supply: 0,
-          ratio: 1,
-          height: await chain.provider.getBlockNumber(),
-          timestamp: Date.now(),
-        };
-      }
-      const thirtyMinutesBlocks = parseInt((chain.blocksPerYear / 365 / 24 / 2).toString());
-      while (snapshots.length < expectedDataPoints) {
-        const newSnapshot: SettSnapshot = {
-          address: sett.settToken,
-          value: 0,
-          balance: 0,
-          supply: 0,
-          ratio: 1,
-          height: lastSnapshot.height - thirtyMinutesBlocks * (interval ?? 1),
-          timestamp: lastSnapshot.timestamp - 30 * 60 * 1000 * (interval ?? 1),
-        };
-        snapshots.push(newSnapshot);
-        lastSnapshot = newSnapshot;
-      }
-    }
 
     return snapshots;
   }
