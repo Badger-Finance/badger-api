7bb9e88f41875a4fc7b226dae170e9649bba1a35
jintao <hellojintao@protonmail.com>
2021-08-24 14:32:26 -0400

    feat: add chain query to rewards

--- src/rewards/rewards.controller.ts
+++ src/rewards/rewards.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Inject, PathParams } from '@tsed/common';
+import { Controller, Get, Inject, PathParams, QueryParams } from '@tsed/common';
 import { ContentType, Description, Hidden, Returns, Summary } from '@tsed/schema';
+import { Chain } from '../chains/config/chain.config';
+import { ChainNetwork } from '../chains/enums/chain-network.enum';
 import { Eligibility } from './interfaces/eligibility.interface';
 import { AirdropMerkleClaim } from './interfaces/merkle-distributor.interface';
 import { RewardMerkleClaimModel } from './interfaces/reward-merkle-claim-model.interface';
@@ -23,8 +25,11 @@ export class RewardController {
   @Description('Return user badger tree reward proof')
   @Returns(200, RewardMerkleClaimModel)
   @(Returns(404).Description('User has no rewards proof available'))
-  async getBadgerTreeReward(@PathParams('address') address: string): Promise<RewardMerkleClaimModel> {
-    return this.rewardsService.getUserRewards(address);
+  async getBadgerTreeReward(
+    @PathParams('address') address: string,
+    @QueryParams('chain') chain?: ChainNetwork,
+  ): Promise<RewardMerkleClaimModel> {
+    return this.rewardsService.getUserRewards(Chain.getChain(chain), address);
   }
 
   @Hidden()
