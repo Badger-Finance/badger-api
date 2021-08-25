f1a2cf0c2880a8ed49b139b1750f2e76abbfc187
jintao <hellojintao@protonmail.com>
2021-08-25 11:50:30 -0400

    test: add accounts coverage

---
+++ src/accounts/accounts.service.spec.ts
@@ -0,0 +1,55 @@
+import { PlatformTest } from '@tsed/common';
+import { Chain } from '../chains/config/chain.config';
+import { Ethereum } from '../chains/config/eth.config';
+import * as accountIndexer from '../indexer/accounts-indexer';
+import { IndexMode } from '../indexer/accounts-indexer';
+import { setupMapper, TEST_ADDR } from '../test/tests.utils';
+import { AccountsService } from './accounts.service';
+import { Account } from './interfaces/account.interface';
+
+describe('charts.service', () => {
+  const chain = new Ethereum();
+  let service: AccountsService;
+  let result: Account;
+  let refreshAccounts: jest.SpyInstance<Promise<void>, [chain: Chain[], mode: IndexMode, accounts: string[]]>;
+
+  const defaultAccount = {
+    address: TEST_ADDR,
+    boost: 2000,
+    boostRank: 3,
+    multipliers: [],
+    value: 320232,
+    earnedValue: 2312,
+    balances: [],
+    claimableBalances: [],
+    nativeBalance: 2033222,
+    nonNativeBalance: 23129,
+  };
+
+  beforeAll(async () => {
+    await PlatformTest.create();
+    service = PlatformTest.get<AccountsService>(AccountsService);
+  });
+
+  beforeEach(async () => {
+    refreshAccounts = jest.spyOn(accountIndexer, 'refreshAccounts').mockImplementation(() => Promise.resolve());
+    setupMapper([defaultAccount]);
+    result = await service.getAccount(chain, TEST_ADDR);
+  });
+
+  afterEach(PlatformTest.reset);
+
+  describe('getAccount', () => {
+    it('calls refresh account on requested address', () => {
+      const requestedAddresses = refreshAccounts.mock.calls.flatMap((calls) => calls[2]);
+      expect(requestedAddresses).toEqual([TEST_ADDR]);
+    });
+    it('returns the expected account', () => {
+      const expectedAccount = {
+        ...defaultAccount,
+        multipliers: {},
+      };
+      expect(result).toMatchObject(expectedAccount);
+    });
+  });
+});
