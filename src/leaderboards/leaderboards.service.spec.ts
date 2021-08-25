8b7a92434603c6427cf84d8a6b291ab1bc8ee72c
jintao <hellojintao@protonmail.com>
2021-08-25 15:09:19 -0400

    test: finish leaderboards resources tests

--- src/leaderboards/leaderboards.service.spec.ts
+++ src/leaderboards/leaderboards.service.spec.ts
@@ -1,9 +1,13 @@
 import { PlatformTest } from '@tsed/common';
+import * as s3Utils from '../aws/s3.utils';
+import { TOKENS } from '../config/tokens.config';
+import { BoostData } from '../rewards/interfaces/boost-data.interface';
 import { setupMapper, TEST_ADDR } from '../test/tests.utils';
 import { LeaderBoardType } from './enums/leaderboard-type.enum';
 import { CachedBoost } from './interface/cached-boost.interface';
 import { LeaderBoardData } from './interface/leaderboard-data.interrface';
 import { LeaderBoardsService } from './leaderboards.service';
+import * as leaderboardUtils from './leaderboards.utils';
 
 describe('leaderboards.service', () => {
   let service: LeaderBoardsService;
@@ -18,35 +22,33 @@ describe('leaderboards.service', () => {
   function randomCachedBoosts(count: number): CachedBoost[] {
     const boosts = [];
     for (let i = 0; i < count; i += 1) {
-      boosts.push(Object.assign(new CachedBoost(), {
-        leaderboard: LeaderBoardType.BadgerBoost,
-        rank: i,
-        address: TEST_ADDR,
-        boost: 2000 - (i * 10),
-        nftMultiplier: 1,
-        stakeRatio: 1 - (i * 0.01),
-        nativeBalance: 100000 / (i + 1),
-        nonNativeBalance: 250000 / (i + 1),
-      }));
+      boosts.push(
+        Object.assign(new CachedBoost(), {
+          leaderboard: LeaderBoardType.BadgerBoost,
+          rank: i + 1,
+          address: TEST_ADDR,
+          boost: 2000 - Math.floor(1 / 2) * 10,
+          nftMultiplier: 1,
+          stakeRatio: 1 - i * 0.01,
+          nativeBalance: 100000 / (i + 1),
+          nonNativeBalance: 250000 / (i + 1),
+        }),
+      );
     }
     return boosts;
   }
 
-  function expectedData(expected: CachedBoost[], page?: number, size?: number): LeaderBoardData {
+  function expectedData(expected: CachedBoost[], total: number, page?: number, size?: number): LeaderBoardData {
     const pageNumber = page || 0;
     const pageSize = size || 20;
-    const offset = pageNumber > 0 ? 1 : 0;
-    const start = pageNumber * pageSize + offset;
-    const end = start + pageSize - offset;
-    const maxPage = parseInt((expected.length / pageSize).toString());
-    const data = expected.slice(start, end);
+    const maxPage = parseInt((total / pageSize).toString());
     return {
-      data,
+      data: expected,
       page: pageNumber,
       size: pageSize,
-      count: data.length,
+      count: expected.length,
       maxPage,
-    }
+    };
   }
 
   describe('loadFullLeaderBoard', () => {
@@ -72,18 +74,96 @@ describe('leaderboards.service', () => {
       it('returns an empty array', async () => {
         setupMapper([]);
         const result = await service.loadLeaderboardEntries();
-        expect(result).toMatchObject(expectedData([]));
+        expect(result).toMatchObject(expectedData([], 0));
       });
     });
     describe('saved 50 leaderboard entries', () => {
       const saved = randomCachedBoosts(50);
-      setupMapper(saved);
       describe('no parameters', () => {
         it('returns an array of first 20 boosts', async () => {
+          const returned = saved.slice(0, 20);
+          setupMapper(returned);
+          jest.spyOn(leaderboardUtils, 'getLeaderBoardSize').mockImplementation(() => Promise.resolve(saved.length));
           const result = await service.loadLeaderboardEntries();
-          expect(result).toMatchObject(saved.slice(20));
+          expect(result).toMatchObject(expectedData(returned, saved.length));
         });
       });
+      describe('requested page parameter', () => {
+        it.each([
+          [0, 0, 20],
+          [1, 21, 40],
+          [2, 41, 50],
+        ])('page %d returns ranks %d through %d', async (page: number, start: number, end: number) => {
+          const returned = saved.slice(start === 0 ? start : start - 1, end);
+          setupMapper(returned);
+          jest.spyOn(leaderboardUtils, 'getLeaderBoardSize').mockImplementation(() => Promise.resolve(saved.length));
+          const result = await service.loadLeaderboardEntries(page);
+          expect(result).toMatchObject(expectedData(returned, saved.length, page));
+        });
+      });
+      describe('requested page and size parameter', () => {
+        it.each([
+          [0, 10, 0, 10],
+          [2, 10, 21, 30],
+          [4, 5, 21, 25],
+          [6, 7, 43, 49],
+          [7, 7, 50, 50],
+        ])('page %d returns ranks %d through %d', async (page: number, size: number, start: number, end: number) => {
+          const returned = saved.slice(start === 0 ? start : start - 1, end);
+          setupMapper(returned);
+          jest.spyOn(leaderboardUtils, 'getLeaderBoardSize').mockImplementation(() => Promise.resolve(saved.length));
+          const result = await service.loadLeaderboardEntries(page, size);
+          expect(result).toMatchObject(expectedData(returned, saved.length, page, size));
+        });
+      });
+    });
+  });
+
+  describe('generateBoostsLeaderBoard', () => {
+    const seeded = randomCachedBoosts(50);
+    const addresses = Object.values(TOKENS);
+    const boostData: BoostData = {
+      userData: Object.fromEntries(
+        seeded.map((cachedBoost, i) => {
+          cachedBoost.address = addresses[i];
+          const boost = {
+            ...cachedBoost,
+            multipliers: {},
+          };
+          return [cachedBoost.address, boost];
+        }),
+      ),
+      multiplierData: {},
+    };
+
+    let response: CachedBoost[];
+    it('indexes all user accounts', async () => {
+      jest
+        .spyOn(s3Utils, 'getObject')
+        .mockImplementation(() => Promise.resolve(Buffer.from(JSON.stringify(boostData), 'utf-8')));
+      response = await LeaderBoardsService.generateBoostsLeaderBoard();
+      expect(response).toMatchObject(seeded);
+    });
+
+    it('sorts ranks by boosts', () => {
+      let last: number | undefined;
+      for (const boost of response) {
+        if (last) {
+          expect(last).toBeLessThan(boost.rank);
+        }
+        last = boost.rank;
+      }
+    });
+
+    // seeded data has 2 of each boost rank
+    it('resovles boost rank ties with stake ratio score', () => {
+      let last: number | undefined;
+      for (const boost of response) {
+        if (last) {
+          expect(last).toBeGreaterThanOrEqual(boost.stakeRatio);
+        }
+        last = boost.stakeRatio;
+      }
     });
   });
 });
