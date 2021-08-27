e60d557f41ffa733d6fcefe8c6c9947d699ff206
jintao <hellojintao@protonmail.com>
2021-08-27 12:36:04 -0400

    chore: break down calls, add cached look ups

--- src/aws/s3.utils.ts
+++ src/aws/s3.utils.ts
@@ -13,24 +13,25 @@ const s3Cache = new NodeCache({ stdTTL: 300, checkperiod: 480 });
  * @param key Key path to object.
  */
 export const getObject = async (bucket: string, key: string): Promise<AWS.S3.Body> => {
+  const s3Path = `s3://${bucket}/${key}`;
   try {
     const params = {
       Bucket: bucket,
       Key: key,
     };
     const objectMetadata = await s3.headObject(params).promise();
-    const cacheKey = objectMetadata.ETag ?? `s3://${bucket}/${key}`;
+    const cacheKey = objectMetadata.ETag ?? s3Path;
     const cachedObject = s3Cache.get(cacheKey) as AWS.S3.GetObjectOutput;
     if (cachedObject) {
       return cachedObject;
     }
     const object = await s3.getObject(params).promise();
     if (!object.Body) {
-      throw new NotFound('Object does not exist');
+      throw new NotFound(`Object ${s3Path} does not exist`);
     }
     s3Cache.set(cacheKey, object.Body);
     return object.Body;
   } catch (err) {
-    throw new BadRequest('Unable to satisfy object request');
+    throw new BadRequest(`Unable to satisfy object request: ${s3Path}`);
   }
 };
--- src/rewards/rewards.utils.ts
+++ src/rewards/rewards.utils.ts
@@ -4,7 +4,10 @@ import { ChainNetwork } from '../chains/enums/chain-network.enum';
 import { REWARD_DATA } from '../config/constants';
 import { RewardMerkleDistribution } from './interfaces/merkle-distributor.interface';
 
-export async function getTreeDistribution(chain: Chain): Promise<RewardMerkleDistribution> {
+export async function getTreeDistribution(chain: Chain): Promise<RewardMerkleDistribution | null> {
+  if (!chain.badgerTree) {
+    return null;
+  }
   const appendChainId = chain.network != ChainNetwork.Ethereum;
   const fileName = `badger-tree${appendChainId ? `-${chain.chainId}` : ''}.json`;
   const rewardFile = await getObject(REWARD_DATA, fileName);
