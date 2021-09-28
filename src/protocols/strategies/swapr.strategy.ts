6753b7ef66e014643a2f3460df525b2e7852dcc3
jintao <hellojintao@protonmail.com>
2021-09-28 15:32:41 -0400

    chore: implement swapr apr

--- src/protocols/strategies/swapr.strategy.ts
+++ src/protocols/strategies/swapr.strategy.ts
@@ -1,11 +1,57 @@
 import { Chain } from '../../chains/config/chain.config';
-import { SWAPR_SUBGRAPH_URL } from '../../config/constants';
+import { ONE_YEAR_SECONDS, SWAPR_SUBGRAPH_URL } from '../../config/constants';
+import { TOKENS } from '../../config/tokens.config';
+import { SwaprStaking__factory } from '../../contracts';
+import { valueSourceToCachedValueSource } from '../../indexer/indexer.utils';
+import { getPrice } from '../../prices/prices.utils';
 import { SettDefinition } from '../../setts/interfaces/sett-definition.interface';
+import { formatBalance, getToken } from '../../tokens/tokens.utils';
 import { CachedValueSource } from '../interfaces/cached-value-source.interface';
+import { uniformPerformance } from '../interfaces/performance.interface';
+import { createValueSource } from '../interfaces/value-source.interface';
+import { tokenEmission } from '../protocols.utils';
 import { getUniV2SwapValue } from './strategy.utils';
 
+const SWAPR_STAKING = {
+  [TOKENS.BARB_SWP_SWAPR_WETH]: '0xe2A7CF0DEB83F2BC2FD15133a02A24B9981f2c17',
+}
+
 export class SwaprStrategy {
-  static async getValueSources(_chain: Chain, settDefinition: SettDefinition): Promise<CachedValueSource[]> {
-    return Promise.all([getUniV2SwapValue(SWAPR_SUBGRAPH_URL, settDefinition)]);
+  static async getValueSources(chain: Chain, settDefinition: SettDefinition): Promise<CachedValueSource[]> {
+    return Promise.all([
+      getUniV2SwapValue(SWAPR_SUBGRAPH_URL, settDefinition),
+      ...(await getSwaprEmission(chain, settDefinition)),
+    ]);
   }
+}
+
+async function getSwaprEmission(chain: Chain, settDefinition: SettDefinition): Promise<CachedValueSource[]> {
+  const stakingContract = SwaprStaking__factory.connect(SWAPR_STAKING[settDefinition.settToken], chain.provider);
+
+  const [duration, totalSupply, lpTokenPrice] = await Promise.all([
+    stakingContract.secondsDuration(),
+    stakingContract.totalStakedTokensAmount(),
+    getPrice(settDefinition.depositToken),
+  ]);
+  const stakedAmount = formatBalance(totalSupply) * lpTokenPrice.usd;
+
+  let sources = [];
+  let tokenIndex = 0;
+  while (true) {
+    try {
+      const rewardInfo = await stakingContract.rewards(tokenIndex);
+      const { token, amount } = rewardInfo;
+      const rewardToken = getToken(token);
+      const rewardTokenPrice = await getPrice(token);
+      const rewardEmission = formatBalance(amount) * rewardTokenPrice.usd;
+      const apr = (((ONE_YEAR_SECONDS / duration.toNumber()) * rewardEmission) / stakedAmount) * 100;
+      const swaprEmission = createValueSource(`${rewardToken.name} Rewards`, uniformPerformance((apr * 0.8) / 2));
+      sources.push(valueSourceToCachedValueSource(swaprEmission, settDefinition, tokenEmission(rewardToken)));
+      tokenIndex++;
+    } catch {
+      break;
+    }
+  }
+
+  return sources;
 }
