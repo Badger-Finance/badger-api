42fa6417e9a95369c1a2a33b8cd9e61a76bdf283
jintao <hellojintao@protonmail.com>
2021-10-24 21:37:10 -0400

    chore: update snapshot

--- src/protocols/strategies/mstable.strategy.ts
+++ src/protocols/strategies/mstable.strategy.ts
@@ -1,5 +1,5 @@
 import { UnprocessableEntity } from '@tsed/exceptions';
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 import { Chain } from '../../chains/config/chain.config';
 import { ONE_YEAR_MS } from '../../config/constants';
 import { TOKENS } from '../../config/tokens.config';
@@ -8,9 +8,9 @@ import { Imbtc__factory } from '../../contracts/factories/Imbtc__factory';
 import { MstableVault__factory } from '../../contracts/factories/MstableVault__factory';
 import { valueSourceToCachedValueSource } from '../../indexer/indexer.utils';
 import { getPrice } from '../../prices/prices.utils';
-import { SourceType } from '../../rewards/enums/source-type.enum';
+// import { SourceType } from '../../rewards/enums/source-type.enum';
 import { SettDefinition } from '../../setts/interfaces/sett-definition.interface';
-import { mStableApiResponse } from '../../tokens/interfaces/mbstable-api-response.interface';
+// import { mStableApiResponse } from '../../tokens/interfaces/mbstable-api-response.interface';
 import { Token } from '../../tokens/interfaces/token.interface';
 import { TokenPrice } from '../../tokens/interfaces/token-price.interface';
 import { formatBalance, getToken } from '../../tokens/tokens.utils';
@@ -19,8 +19,8 @@ import { uniformPerformance } from '../interfaces/performance.interface';
 import { createValueSource } from '../interfaces/value-source.interface';
 import { tokenEmission } from '../protocols.utils';
 
-const MSTABLE_API_URL = 'https://api.mstable.org/';
-const MSTABLE_BTC_APR = `${MSTABLE_API_URL}/massets/mbtc`;
+// const MSTABLE_API_URL = 'https://api.mstable.org/';
+// const MSTABLE_BTC_APR = `${MSTABLE_API_URL}/massets/mbtc`;
 const MSTABLE_MBTC_VAULT = '0xF38522f63f40f9Dd81aBAfD2B8EFc2EC958a3016';
 const MSTABLE_HMBTC_VAULT = '0xF65D53AA6e2E4A5f4F026e73cb3e22C22D75E35C';
 
@@ -67,7 +67,7 @@ export async function getMhBtcPrice(chain: Chain, token: Token): Promise<TokenPr
 }
 
 async function getImBtcValuceSource(chain: Chain, settDefinition: SettDefinition): Promise<CachedValueSource[]> {
-  // getMAssetValueSource(settDefinition), 
+  // getMAssetValueSource(settDefinition),
   return Promise.all([getVaultSource(chain, settDefinition, MSTABLE_MBTC_VAULT)]);
 }
 
@@ -108,28 +108,28 @@ async function getVaultSource(
 }
 
 // TODO: re-enable mstable api at some point
-async function getMAssetValueSource(settDefinition: SettDefinition): Promise<CachedValueSource> {
-  const sourceName = 'mBTC Native Yield';
-  const response = await fetch(MSTABLE_BTC_APR);
-  const performance = uniformPerformance(0);
-  if (!response.ok) {
-    return valueSourceToCachedValueSource(
-      createValueSource(sourceName, performance),
-      settDefinition,
-      SourceType.Emission,
-    );
-  }
-  const results: mStableApiResponse = await response.json();
-  const data = results.mbtc.metrics.historic.reverse().slice(0, 30);
-  let totalApy = 0;
-  for (let i = 0; i < data.length; i++) {
-    totalApy += data[i].dailyAPY;
-    const currentApy = totalApy / (i + 1);
-    if (i === 0) performance.oneDay = currentApy;
-    if (i === 2) performance.threeDay = currentApy;
-    if (i === 6) performance.sevenDay = currentApy;
-    if (i === 29) performance.thirtyDay = currentApy;
-  }
-  const valueSource = createValueSource(sourceName, performance);
-  return valueSourceToCachedValueSource(valueSource, settDefinition, SourceType.Emission);
-}
+// async function getMAssetValueSource(settDefinition: SettDefinition): Promise<CachedValueSource> {
+//   const sourceName = 'mBTC Native Yield';
+//   const response = await fetch(MSTABLE_BTC_APR);
+//   const performance = uniformPerformance(0);
+//   if (!response.ok) {
+//     return valueSourceToCachedValueSource(
+//       createValueSource(sourceName, performance),
+//       settDefinition,
+//       SourceType.Emission,
+//     );
+//   }
+//   const results: mStableApiResponse = await response.json();
+//   const data = results.mbtc.metrics.historic.reverse().slice(0, 30);
+//   let totalApy = 0;
+//   for (let i = 0; i < data.length; i++) {
+//     totalApy += data[i].dailyAPY;
+//     const currentApy = totalApy / (i + 1);
+//     if (i === 0) performance.oneDay = currentApy;
+//     if (i === 2) performance.threeDay = currentApy;
+//     if (i === 6) performance.sevenDay = currentApy;
+//     if (i === 29) performance.thirtyDay = currentApy;
+//   }
+//   const valueSource = createValueSource(sourceName, performance);
+//   return valueSourceToCachedValueSource(valueSource, settDefinition, SourceType.Emission);
+// }
