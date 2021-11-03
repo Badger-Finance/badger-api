2026f66401d1115d36ba1963ecccc5b9a18cd20a
jintao <hellojintao@protonmail.com>
2021-11-03 18:19:05 -0400

    chore: update indexer tests

--- src/indexer/accounts-indexer.ts
+++ src/indexer/accounts-indexer.ts
@@ -1,10 +1,5 @@
 import { BigNumber, ethers } from 'ethers';
-import {
-  getAccounts,
-  getCachedAccount,
-  getUserAccounts,
-  toSettBalance,
-} from '../accounts/accounts.utils';
+import { getAccounts, getCachedAccount, getUserAccounts, toSettBalance } from '../accounts/accounts.utils';
 import { AccountMap } from '../accounts/interfaces/account-map.interface';
 import { CachedAccount } from '../accounts/interfaces/cached-account.interface';
 import { CachedBalance } from '../accounts/interfaces/cached-claimable-balance.interface';
