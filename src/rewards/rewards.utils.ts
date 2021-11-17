7e200500dad2bd1fac62e227214f4697827c279d
mitche50 <mitche50@gmail.com>
2021-11-17 16:06:53 -0500

    chore: only blacklist on eth

--- src/rewards/rewards.utils.ts
+++ src/rewards/rewards.utils.ts
@@ -149,19 +149,22 @@ export async function getRewardEmission(chain: Chain, settDefinition: SettDefini
    *
    */
 
-  // Badger controlled addresses are blacklisted from receiving rewards.
-  const accountService = new AccountsService();
-  const blacklistedAccounts = await Promise.all([
-    accountService.getAccount(chain, '0xB65cef03b9B89f99517643226d76e286ee999e77'), // dev multisig
-    accountService.getAccount(chain, '0x86cbD0ce0c087b482782c181dA8d191De18C8275'), // tech ops multisig
-    accountService.getAccount(chain, '0x042B32Ac6b453485e357938bdC38e0340d4b9276'), // treasury ops multisig
-    accountService.getAccount(chain, '0xD0A7A8B98957b9CD3cFB9c0425AbE44551158e9e'), // treasury vault
-  ]);
+  // Badger controlled addresses are blacklisted from receiving rewards. We only dogfood on ETH
+  let BLACKLISTED_TVL = 0;
+  if (chain.chainId === '1') {
+    const accountService = new AccountsService();
+    const blacklistedAccounts = await Promise.all([
+      accountService.getAccount(chain, '0xB65cef03b9B89f99517643226d76e286ee999e77'), // dev multisig
+      accountService.getAccount(chain, '0x86cbD0ce0c087b482782c181dA8d191De18C8275'), // tech ops multisig
+      accountService.getAccount(chain, '0x042B32Ac6b453485e357938bdC38e0340d4b9276'), // treasury ops multisig
+      accountService.getAccount(chain, '0xD0A7A8B98957b9CD3cFB9c0425AbE44551158e9e'), // treasury vault
+    ]);
 
-  const initial = 0;
-  const BLACKLISTED_TVL = blacklistedAccounts.reduce((pv, cv) => {
-    return pv + cv.data[settToken].balance;
-  }, initial);
+    const initial = 0;
+    BLACKLISTED_TVL = blacklistedAccounts.reduce((pv, cv) => {
+      return pv + cv.data[settToken].balance;
+    }, initial);
+  }
 
   const emissionSources = [];
   for (const schedule of activeSchedules) {
