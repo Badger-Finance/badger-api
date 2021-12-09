918a9bf278b25be33e2d08940026267813f593da
jintao <hellojintao@protonmail.com>
2021-12-08 21:49:27 -0500

    feat: add pretty quick + husky precommits

--- src/leaderboards/leaderboards.service.ts
+++ src/leaderboards/leaderboards.service.ts
@@ -1,20 +1,10 @@
-import { BadgerTypeMap, LeaderBoardData, LeaderboardSummary } from '@badger-dao/sdk';
+import { BadgerTypeMap, LeaderboardSummary } from '@badger-dao/sdk';
 import { Service } from '@tsed/common';
 import { Chain } from '../chains/config/chain.config';
-import { CachedBoost } from './interface/cached-boost.interface';
-import {
-  getFullLeaderBoard,
-  getLeaderBoardEntryRange,
-  getLeaderBoardSize,
-  queryLeaderboardSummary,
-} from './leaderboards.utils';
+import { queryLeaderboardSummary } from './leaderboards.utils';
 
 @Service()
 export class LeaderBoardsService {
-  async loadFullLeaderBoard(chain: Chain): Promise<CachedBoost[]> {
-    return getFullLeaderBoard(chain);
-  }
-
   async fetchLeaderboardSummary(chain: Chain): Promise<LeaderboardSummary> {
     const cachedSummary = await queryLeaderboardSummary(chain);
     const summary = Object.fromEntries(
@@ -24,33 +14,5 @@ export class LeaderBoardsService {
       summary,
       updatedAt: cachedSummary.updatedAt,
     };
-  }
-
-  async loadLeaderboardEntries(chain: Chain, page?: number, size?: number): Promise<LeaderBoardData> {
-    const pageNumber = page || 0;
-    const pageSize = size || 20;
-    const offset = pageNumber > 0 ? 1 : 0;
-    const start = pageNumber * pageSize + offset;
-    const end = start + pageSize - offset;
-    try {
-      const [data, size] = await Promise.all([getLeaderBoardEntryRange(chain, start, end), getLeaderBoardSize(chain)]);
-      const maxPage = parseInt((size / pageSize).toString());
-      return {
-        data,
-        page: pageNumber,
-        size: pageSize,
-        count: data.length,
-        maxPage,
-      };
-    } catch (err) {
-      console.error(err);
-      return {
-        data: [],
-        page: 0,
-        size: 0,
-        count: 0,
-        maxPage: 0,
-      };
-    }
   }
 }
