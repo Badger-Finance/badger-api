6ae6be07311847cd2b3b721ec797bc46d64a325a
Andrew Kulikov <jadecoresky@gmail.com>
2021-12-17 12:51:07 +0200

    chore: add test to check if mapper is called for updating token with balance

--- src/indexers/token-balances-indexer.spec.ts
+++ src/indexers/token-balances-indexer.spec.ts
@@ -3,6 +3,8 @@ import { updateTokenBalance } from './token-balances-indexer';
 import { Ethereum } from '../chains/config/eth.config';
 import { getVaultDefinition } from '../vaults/vaults.utils';
 import { TOKENS } from '../config/tokens.config';
+import { CachedLiquidityPoolTokenBalance } from '../tokens/interfaces/cached-liquidity-pool-token-balance.interface';
+import { CachedTokenBalance } from '../tokens/interfaces/cached-token-balance.interface';
 
 describe('token-balances-indexer', () => {
   const chain = new Ethereum();
@@ -11,9 +13,40 @@ describe('token-balances-indexer', () => {
     put = jest.spyOn(DataMapper.prototype, 'put').mockImplementation();
   });
   describe('updateTokenBalance', () => {
-    it('should not update anything for token without balance', async () => {
+    it('should not update for token without balance', async () => {
       await updateTokenBalance(chain, getVaultDefinition(chain, TOKENS.BDIGG));
       expect(put.mock.calls.length).toEqual(0);
+    });
+    it('should not update for lp token wihtout balance', async () => {
+      await updateTokenBalance(chain, getVaultDefinition(chain, TOKENS.BDIGG));
+      expect(put.mock.calls.length).toEqual(0);
+    });
+    it('should update token with balance', async () => {
+      await updateTokenBalance(
+        chain,
+        Object.assign({
+          name: 'something',
+          depositToken: TOKENS.CRV_HBTC,
+          getTokenBalance: async () =>
+            Object.assign(new CachedLiquidityPoolTokenBalance(), {
+              id: '123',
+              pairId: '123',
+              protocol: 'some protocol',
+              tokenBalances: [
+                Object.assign(new CachedTokenBalance(), {
+                  address: TOKENS.BCRV_HTBC,
+                  name: 'BCRV_HBTC',
+                  symbol: 'BCRVHBTC',
+                  decimals: 18,
+                  balance: 100000000,
+                  valueEth: 1,
+                  valueUsd: 10101010,
+                }),
+              ],
+            }),
+        }),
+      );
+      expect(put.mock.calls.length).toEqual(1);
     });
   });
 });
