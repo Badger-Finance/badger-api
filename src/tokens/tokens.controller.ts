0961bcfdde04df5fb1c275ac07055ee983d179aa
hunson <functionsilence@gmail.com>
2022-03-19 19:36:24 +0200

    fix: review upds

--- src/tokens/tokens.controller.ts
+++ src/tokens/tokens.controller.ts
@@ -3,6 +3,8 @@ import { Controller, Get, QueryParams, UseCache } from '@tsed/common';
 import { ContentType, Description, Returns, Summary } from '@tsed/schema';
 import { Chain } from '../chains/config/chain.config';
 import { TokenConfigModel } from './interfaces/token-config-model.interface';
+import { getFullTokens } from './tokens.utils';
+import { TokenFullMap } from './interfaces/token-full.interface';
 
 @Controller('/tokens')
 export class TokensController {
@@ -12,10 +14,9 @@ export class TokensController {
   @Summary('Get a summary of tokens related to the Badger Protocol')
   @Returns(200, TokenConfigModel)
   @Description('Return a map of checksum contract address to token information.')
-  async listSetts(@QueryParams('chain') chain?: Network): Promise<TokenConfigModel> {
+  async getTokens(@QueryParams('chain') chain?: Network): Promise<TokenFullMap> {
     const requestChain = Chain.getChain(chain);
     const chainTokens = Object.keys(requestChain.tokens);
-    const sdk = await requestChain.getSdk();
-    return sdk.tokens.loadTokens(chainTokens);
+    return getFullTokens(requestChain, chainTokens);
   }
 }
