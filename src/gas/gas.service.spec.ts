32f11e616b59ba7dbae972ca3aa0b9cca8a3c9ee
kingikalgo <kingikalgo@protonmail.com>
2022-04-04 22:46:55 -0500

    test(badger-api): Add `gas.service.spec.ts`

---
+++ src/gas/gas.service.spec.ts
@@ -0,0 +1,41 @@
+import { PlatformTest } from '@tsed/common';
+import { Ethereum } from '../chains/config/eth.config';
+import { setupChainGasPrices } from '../test/tests.utils';
+import { GasService } from './gas.service';
+
+describe('GasService', () => {
+  const chain = new Ethereum();
+  let service: GasService;
+
+  beforeAll(async () => {
+    await PlatformTest.create();
+    service = PlatformTest.get<GasService>(GasService);
+  });
+
+  afterAll(PlatformTest.reset);
+
+  describe('getGasPrices', () => {
+    it('returns gas prices for a chain', async () => {
+      setupChainGasPrices();
+      const gasPrices = await service.getGasPrices(chain);
+      expect(gasPrices).toMatchObject({
+        fast: {
+          maxFeePerGas: expect.any(Number),
+          maxPriorityFeePerGas: expect.any(Number),
+        },
+        rapid: {
+          maxFeePerGas: expect.any(Number),
+          maxPriorityFeePerGas: expect.any(Number),
+        },
+        slow: {
+          maxFeePerGas: expect.any(Number),
+          maxPriorityFeePerGas: expect.any(Number),
+        },
+        standard: {
+          maxFeePerGas: expect.any(Number),
+          maxPriorityFeePerGas: expect.any(Number),
+        },
+      });
+    });
+  });
+});
