c2ca2d3bdf7637d0a831e7993d429ea76ba8c547
namelsking <functionsilence@gmail.com>
2022-04-26 11:56:32 +0300

    add model resp examples

--- src/citadel/interfaces/citadel-reward-event-model.interface.ts
+++ src/citadel/interfaces/citadel-reward-event-model.interface.ts
@@ -1,25 +1,29 @@
-import { Description, Property, Title } from '@tsed/schema';
+import { Description, Example, Property, Title } from '@tsed/schema';
 import { CitadelRewardEvent } from './citadel-reward-event.interface';
 
 @Description('Citadel DAO Reward Event')
 export class CitadelRewardEventModel implements CitadelRewardEvent {
   @Title('block')
   @Description('At which reward was added')
+  @Example('11123943')
   @Property()
   block?: number;
 
   @Title('user')
   @Description('Acc addr on the network')
+  @Example('0x19d97d8fa813ee2f51ad4b4e04ea08baf4dffc28')
   @Property()
   user?: string;
 
   @Title('token')
   @Description('Token addr reward was given')
+  @Example('0xaF0b1FDf9c6BfeC7b3512F207553c0BA00D7f1A2')
   @Property()
   token: string;
 
   @Title('amount')
   @Description('Value of roken reward')
+  @Example('100.2341')
   @Property()
   amount: number;
 
