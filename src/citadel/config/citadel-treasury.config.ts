02cd28f47402e65f4cfd6a13a253083349f8a588
jintao <hellojintao@protonmail.com>
2022-06-02 09:46:49 -0400

    chore: update citdael treasury + support fxs

--- src/citadel/config/citadel-treasury.config.ts
+++ src/citadel/config/citadel-treasury.config.ts
@@ -11,4 +11,4 @@ vaults.forEach((v) => tokens.delete(v));
 export const TRACKED_TOKENS = [...tokens];
 export const TRACKED_VAULTS = [...vaults];
 
-export const CITADEL_TREASURY_ADDRESS = '0x38724146C8dc1Aa49c3395091cf86B789c37F52c';
+export const CITADEL_TREASURY_ADDRESS = '0xb7Bf6c956da0f013BC59ecDB0748f73d0473cd3a';
