3019d9470a92b7bdfa9ba44989a96f663a2500ba
jintao <hellojintao@protonmail.com>
2022-08-09 15:26:52 -0400

    test: add projected yield coverage

--- src/vaults/yields.utils.spec.ts
+++ src/vaults/yields.utils.spec.ts
@@ -1,15 +1,32 @@
-import { ONE_DAY_MS } from '@badger-dao/sdk';
+import { ONE_DAY_MS, VaultState } from '@badger-dao/sdk';
 
 import { TOKENS } from '../config/tokens.config';
 import { SourceType } from '../rewards/enums/source-type.enum';
-import { MOCK_VAULT_DEFINITION } from '../test/constants';
+import { MOCK_VAULT, MOCK_VAULT_DEFINITION } from '../test/constants';
 import { setupMapper } from '../test/tests.utils';
 import { fullTokenMockMap } from '../tokens/mocks/full-token.mock';
 import { mockBalance } from '../tokens/tokens.utils';
 import { VAULT_SOURCE } from './vaults.utils';
-import { calculateBalanceDifference, calculateYield, createYieldSource, getYieldSources } from './yields.utils';
+import {
+  calculateBalanceDifference,
+  calculateYield,
+  createYieldSource,
+  getVaultYieldProjection,
+  getYieldSources,
+} from './yields.utils';
 
 describe('yields.utils', () => {
+  const baseMockSources = [
+    createYieldSource(MOCK_VAULT_DEFINITION, SourceType.PreCompound, VAULT_SOURCE, 7),
+    createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Compound, VAULT_SOURCE, 11),
+    createYieldSource(MOCK_VAULT_DEFINITION, SourceType.TradeFee, 'LP Fees', 2),
+    createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Emission, 'Badger', 4),
+    createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Emission, 'Boosted Badger', 6, { min: 0.5, max: 2 }),
+    createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Flywheel, 'Vault Flywheel', 5),
+    createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Distribution, 'Badger', 3),
+    createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Distribution, 'Irrelevant', 0.0001),
+  ];
+
   describe('calculateYield', () => {
     it.each([
       [365, 1, ONE_DAY_MS, 0, 100],
@@ -17,12 +34,19 @@ describe('yields.utils', () => {
       [365, 2, ONE_DAY_MS, 0, 200],
       [365, 1, ONE_DAY_MS, 365, 171.45674820219733],
       [365, 1, ONE_DAY_MS, 180, 114.37716231252146],
+      [0, 1, ONE_DAY_MS, 0, 0],
     ])(
       '%d earned %d over %d ms with %d compounded, for %d apr',
       (principal, earned, duration, compoundingValue, expected) => {
         expect(calculateYield(principal, earned, duration, compoundingValue)).toEqual(expected);
       },
     );
+
+    it('throws an error when provided with invalid principal and compounding pair', () => {
+      expect(() => calculateYield(365, 1, ONE_DAY_MS, 380)).toThrow(
+        'Compounding value must be less than or equal to principal',
+      );
+    });
   });
 
   describe('calculateBalanceDifference', () => {
@@ -37,18 +61,52 @@ describe('yields.utils', () => {
 
   describe('getYieldSources', () => {
     it('returns vault yield sources categorized by required breakdown', async () => {
+      setupMapper(baseMockSources);
+      const yieldSources = await getYieldSources(MOCK_VAULT_DEFINITION);
+      expect(yieldSources).toMatchSnapshot();
+    });
+
+    it('returns only passive yield sources for a discontinued vault', async () => {
       const mockSources = [
         createYieldSource(MOCK_VAULT_DEFINITION, SourceType.PreCompound, VAULT_SOURCE, 7),
         createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Compound, VAULT_SOURCE, 11),
         createYieldSource(MOCK_VAULT_DEFINITION, SourceType.TradeFee, 'LP Fees', 2),
-        createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Emission, 'Badger', 4),
-        createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Emission, 'Boosted Badger', 6, { min: 0.5, max: 2 }),
         createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Flywheel, 'Vault Flywheel', 5),
         createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Distribution, 'Badger', 3),
+        createYieldSource(MOCK_VAULT_DEFINITION, SourceType.Distribution, 'Irrelevant', 0.0001),
       ];
       setupMapper(mockSources);
-      const yieldSources = await getYieldSources(MOCK_VAULT_DEFINITION);
+      const definitionCopy = JSON.parse(JSON.stringify(MOCK_VAULT_DEFINITION));
+      definitionCopy.state = VaultState.Discontinued;
+      const yieldSources = await getYieldSources(definitionCopy);
       expect(yieldSources).toMatchSnapshot();
+    });
+  });
+
+  describe('getVaultYieldProjection', () => {
+    it('returns a yield projection estimation from given inputs', async () => {
+      const mockDefinition = JSON.parse(JSON.stringify(MOCK_VAULT_DEFINITION));
+      mockDefinition.address = MOCK_VAULT.vaultToken;
+      setupMapper(baseMockSources);
+      jest.spyOn(Date, 'now').mockImplementation(() => referenceTime);
+      const yieldSources = await getYieldSources(mockDefinition);
+      const referenceTime = 1660071985202;
+      const wbtc = fullTokenMockMap[TOKENS.WBTC];
+      const mockVault = JSON.parse(JSON.stringify(MOCK_VAULT));
+      mockVault.lastHarvest = referenceTime - ONE_DAY_MS * 30;
+      const mockYieldEstimate = {
+        vault: MOCK_VAULT.vaultToken,
+        yieldTokens: [mockBalance(wbtc, 0.0092)],
+        harvestTokens: [mockBalance(wbtc, 0.0091)],
+        previousYieldTokens: [mockBalance(wbtc, 0.0048)],
+        previousHarvestTokens: [mockBalance(wbtc, 0.0047)],
+        duration: ONE_DAY_MS * 15,
+        lastMeasuredAt: referenceTime - ONE_DAY_MS * 15,
+        lastReportedAt: 0,
+        lastHarvestedAt: referenceTime - ONE_DAY_MS * 30,
+      };
+      const result = getVaultYieldProjection(mockVault, yieldSources, mockYieldEstimate);
+      expect(result).toMatchSnapshot();
     });
   });
 });
