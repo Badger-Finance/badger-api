cf253229cb33d0f0f2b4aa0c5a9bde8f5db37dd3
namelsking <functionsilence@gmail.com>
2022-08-13 20:34:54 +0300

    chore: review fixes

--- src/indexers/vault-definition-indexer.spec.ts
+++ src/indexers/vault-definition-indexer.spec.ts
@@ -5,94 +5,95 @@ import graphVaults from '@badger-dao/sdk-mocks/generated/ethereum/graph/loadSett
 import registryVaults from '@badger-dao/sdk-mocks/generated/ethereum/vaults/loadVaults.json';
 
 import { SUPPORTED_CHAINS } from '../chains/chain';
+import { TEST_CURRENT_TIMESTAMP } from '../test/constants';
 import { mockSupportedChains, setupMapper, setupVaultsCoumpoundDDB } from '../test/tests.utils';
 import { captureVaultData } from './vault-definition-indexer';
 
-describe('captureVaultData task', () => {
-  const TIME_NOW_MS = 1660381887554;
+describe('vault-definition-indexer', () => {
+  describe('captureVaultData', () => {
+    let put: jest.SpyInstance<Promise<StringToAnyObjectMap>, [items: PutParameters<StringToAnyObjectMap>]>;
 
-  let put: jest.SpyInstance<Promise<StringToAnyObjectMap>, [items: PutParameters<StringToAnyObjectMap>]>;
+    beforeEach(async () => {
+      jest.spyOn(Date, 'now').mockImplementation(() => TEST_CURRENT_TIMESTAMP);
+      jest.spyOn(console, 'error').mockImplementation();
+      jest.spyOn(console, 'warn').mockImplementation();
 
-  beforeEach(async () => {
-    jest.spyOn(Date, 'now').mockImplementation(() => TIME_NOW_MS);
-    jest.spyOn(console, 'error').mockImplementation();
-    jest.spyOn(console, 'warn').mockImplementation();
+      setupMapper([]);
 
-    setupMapper([]);
+      jest.spyOn(VaultsService.prototype, 'loadVaults').mockImplementation(async function () {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if ((<BadgerSDK>this).config.network != 'ethereum') return [];
+        return <RegistryVault[]>registryVaults;
+      });
+      jest.spyOn(BadgerGraph.prototype, 'loadSett').mockImplementation(async ({ id }) => {
+        const graphVault = graphVaults.setts.find((v) => v.id === id);
 
-    jest.spyOn(VaultsService.prototype, 'loadVaults').mockImplementation(async function () {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      if ((<BadgerSDK>this).config.network != 'ethereum') return [];
-      return <RegistryVault[]>registryVaults;
-    });
-    jest.spyOn(BadgerGraph.prototype, 'loadSett').mockImplementation(async ({ id }) => {
-      const graphVault = graphVaults.setts.find((v) => v.id === id);
+        if (!graphVault) return { sett: null };
 
-      if (!graphVault) return { sett: null };
+        return { sett: <gqlGenT.SettQuery['sett']>graphVault, __typename: 'Query' };
+      });
 
-      return { sett: <gqlGenT.SettQuery['sett']>graphVault, __typename: 'Query' };
-    });
+      await mockSupportedChains();
 
-    await mockSupportedChains();
-
-    put = jest.spyOn(DataMapper.prototype, 'put').mockImplementation();
-  });
-
-  it('should construct and save vaults definitions to ddb', async () => {
-    await captureVaultData();
-
-    expect(put.mock.calls.length).toBe(24);
-  });
-
-  it('should skip, and warn if no vault was fetched from registry', async () => {
-    jest.spyOn(VaultsService.prototype, 'loadVaults').mockImplementation(async () => []);
-
-    const consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
-
-    await captureVaultData();
-
-    expect(consoleWarnMock.mock.calls.length).toBe(SUPPORTED_CHAINS.length);
-    expect(put.mock.calls.length).toBe(0);
-  });
-
-  it('should skip, and warn if no vault was fetched from the graph', async () => {
-    jest.spyOn(BadgerGraph.prototype, 'loadSett').mockImplementation(async () => ({ sett: null }));
-
-    const consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
-
-    await captureVaultData();
-
-    expect(consoleWarnMock.mock.calls.length).toBe(29);
-    expect(put.mock.calls.length).toBe(0);
-  });
-
-  it('should mark vault in ddb and not in registryVaults response as isProduction = 0', async () => {
-    const savedProductionValues = [];
-    const savedVaults = [registryVaults[0], registryVaults[1], registryVaults[2]];
-
-    jest.spyOn(VaultsService.prototype, 'loadVaults').mockImplementation(async function () {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      if ((<BadgerSDK>this).config.network != 'ethereum') return [];
-      return <RegistryVault[]>registryVaults.filter((v) => !savedVaults.map((v) => v.address).includes(v.address));
+      put = jest.spyOn(DataMapper.prototype, 'put').mockImplementation();
     });
 
-    setupVaultsCoumpoundDDB((v) => {
-      return savedVaults.map((v) => v.address).includes(v.address);
+    it('should construct and save vaults definitions to ddb', async () => {
+      await captureVaultData();
+
+      expect(put.mock.calls.length).toBe(24);
     });
 
-    jest.spyOn(DataMapper.prototype, 'put').mockImplementation(async (inst) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      if (savedVaults.map((v) => v.address).includes(inst.address) && inst.isProduction === 0) {
-        savedProductionValues.push(true);
-      }
-      return inst;
+    it('should skip, and warn if no vault was fetched from registry', async () => {
+      jest.spyOn(VaultsService.prototype, 'loadVaults').mockImplementation(async () => []);
+
+      const consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
+
+      await captureVaultData();
+
+      expect(consoleWarnMock.mock.calls.length).toBe(SUPPORTED_CHAINS.length);
+      expect(put.mock.calls.length).toBe(0);
     });
 
-    await captureVaultData();
+    it('should skip, and warn if no vault was fetched from the graph', async () => {
+      jest.spyOn(BadgerGraph.prototype, 'loadSett').mockImplementation(async () => ({ sett: null }));
 
-    expect(savedProductionValues.length).toBe(savedVaults.length);
+      const consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
+
+      await captureVaultData();
+
+      expect(consoleWarnMock.mock.calls.length).toBe(29);
+      expect(put.mock.calls.length).toBe(0);
+    });
+
+    it('should mark vault in ddb and not in registryVaults response as isProduction = 0', async () => {
+      const savedProductionValues = [];
+      const savedVaults = [registryVaults[0], registryVaults[1], registryVaults[2]];
+
+      jest.spyOn(VaultsService.prototype, 'loadVaults').mockImplementation(async function () {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if ((<BadgerSDK>this).config.network != 'ethereum') return [];
+        return <RegistryVault[]>registryVaults.filter((v) => !savedVaults.map((v) => v.address).includes(v.address));
+      });
+
+      setupVaultsCoumpoundDDB((v) => {
+        return savedVaults.map((v) => v.address).includes(v.address);
+      });
+
+      jest.spyOn(DataMapper.prototype, 'put').mockImplementation(async (inst) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (savedVaults.map((v) => v.address).includes(inst.address) && inst.isProduction === 0) {
+          savedProductionValues.push(true);
+        }
+        return inst;
+      });
+
+      await captureVaultData();
+
+      expect(savedProductionValues.length).toBe(savedVaults.length);
+    });
   });
 });
