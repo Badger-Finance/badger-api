0a7d3cca9f6f722231640f2f7392369b72a42b11
jintao <hellojintao@protonmail.com>
2022-08-15 12:49:03 -0400

    chore: update workflows + update indexer

--- src/indexers/protocol-metric-indexer.ts
+++ src/indexers/protocol-metric-indexer.ts
@@ -1,11 +1,13 @@
+import { SUPPORTED_CHAINS } from "src/chains/chain";
+
 import { getDataMapper } from "../aws/dynamodb.utils";
 import { ProtocolMetricSnapshot } from "../aws/models/protocol-metric-snapshot.model";
 import { MetricType } from "../metrics/enums/metric-type";
-import { getProtocolMetrics } from "../metrics/metrics.utils";
+import { getChainMetrics } from "../metrics/metrics.utils";
 
 export const indexProtocolMetrics = async () => {
   const mapper = getDataMapper();
-  const metric = await getProtocolMetrics();
+  const metric = await getChainMetrics(SUPPORTED_CHAINS);
   const metricSnapshot = Object.assign(new ProtocolMetricSnapshot(), { ...metric, type: MetricType.protocol });
 
   try {
