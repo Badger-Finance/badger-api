16253909ac70b8186ec7dce5fa51bac8c712ad5f
jintao <hellojintao@protonmail.com>
2022-08-24 14:40:50 -0400

    chore: remove bad check

--- src/indexers/yield-estimates-indexer.ts
+++ src/indexers/yield-estimates-indexer.ts
@@ -1,14 +1,16 @@
-import BadgerSDK, { ONE_DAY_MS, Protocol, TokenBalance, VaultState, VaultVersion } from '@badger-dao/sdk';
+import BadgerSDK, { ONE_DAY_MS, TokenBalance, VaultState, VaultVersion } from '@badger-dao/sdk';
 import {
   BadgerTreeDistribution_OrderBy,
   OrderDirection,
   SettHarvest_OrderBy,
 } from '@badger-dao/sdk/lib/graphql/generated/badger';
+import { UnprocessableEntity } from '@tsed/exceptions';
 
 import { getDataMapper } from '../aws/dynamodb.utils';
 import { VaultDefinitionModel } from '../aws/models/vault-definition.model';
 import { YieldEstimate } from '../aws/models/yield-estimate.model';
 import { getSupportedChains } from '../chains/chains.utils';
+import { Chain } from '../chains/config/chain.config';
 import { CachedTokenBalance } from '../tokens/interfaces/cached-token-balance.interface';
 import { calculateBalanceDifference, toTokenValue } from '../tokens/tokens.utils';
 import { VAULT_SOURCE } from '../vaults/vaults.config';
@@ -56,116 +58,133 @@ async function loadGraphTimestamp(sdk: BadgerSDK, vault: VaultDefinitionModel): 
   return lastHarvestedAt;
 }
 
+async function applyProtocolFees(chain: Chain, vault: VaultDefinitionModel, yieldEstimate: YieldEstimate) {
+  const cachedVault = await getCachedVault(chain, vault);
+  const {
+    strategy: { performanceFee },
+  } = cachedVault;
+  // max fee bps is 10_000, this scales values by the remainder after fees
+  const feeMultiplier = 1 - performanceFee / 10_000;
+  const feeScalingFunction = (t: CachedTokenBalance) => {
+    t.balance *= feeMultiplier;
+    t.value *= feeMultiplier;
+  };
+  yieldEstimate.harvestTokens.forEach(feeScalingFunction);
+  yieldEstimate.yieldTokens.forEach(feeScalingFunction);
+  yieldEstimate.harvestTokens.forEach((t) => {
+    if (t.address === vault.depositToken) {
+      t.name = VAULT_SOURCE;
+    }
+  });
+
+  const harvestDifference = calculateBalanceDifference(
+    yieldEstimate.previousHarvestTokens,
+    yieldEstimate.harvestTokens,
+  );
+  const hasNegatives = harvestDifference.some((b) => b.balance < 0);
+
+  // if the difference incur negative values due to slippage or otherwise, force a comparison against the full harvest
+  if (hasNegatives) {
+    yieldEstimate.previousHarvestTokens = [];
+    console.warn(`${vault.name} flashed negative balance earnings!`);
+  }
+
+  console.log(`${vault.name} Yield Estimates`);
+  console.log('Current Yield Tokens');
+  console.table(yieldEstimate.yieldTokens.map(toTableRow));
+  console.log('Previous Yield Tokens');
+  console.table(yieldEstimate.previousYieldTokens.map(toTableRow));
+  console.log('Current Harvest Tokens');
+  console.table(yieldEstimate.harvestTokens.map(toTableRow));
+  console.log('Previous Harvest Tokens');
+  console.table(yieldEstimate.previousHarvestTokens.map(toTableRow));
+}
+
+function defaultEstimate(vault: VaultDefinitionModel, existingEstimate: YieldEstimate): YieldEstimate {
+  return {
+    vault: vault.address,
+    yieldTokens: [],
+    harvestTokens: [],
+    lastHarvestedAt: 0,
+    previousYieldTokens: existingEstimate.yieldTokens,
+    previousHarvestTokens: existingEstimate.harvestTokens,
+    lastMeasuredAt: existingEstimate.lastMeasuredAt ?? 0,
+    duration: existingEstimate.duration ?? Number.MAX_SAFE_INTEGER,
+    lastReportedAt: existingEstimate.lastReportedAt ?? 0,
+  };
+}
+
+async function captureVaultReports(chain: Chain, vault: VaultDefinitionModel, now: number) {
+  const convert = async (t: TokenBalance) => toTokenValue(chain, t);
+  try {
+    const sdk = await chain.getSdk();
+    const mapper = getDataMapper();
+    const existingEstimate = await queryYieldEstimate(vault);
+    const yieldEstimate = defaultEstimate(vault, existingEstimate);
+
+    let shouldCheckGraph = true;
+
+    if (vault.version === VaultVersion.v1_5) {
+      try {
+        const pendingHarvest = await sdk.vaults.getPendingHarvest(vault.address);
+        yieldEstimate.harvestTokens = await Promise.all(pendingHarvest.tokenRewards.map(convert));
+        yieldEstimate.lastHarvestedAt = pendingHarvest.lastHarvestedAt * 1000;
+        shouldCheckGraph = false;
+      } catch {
+        if (now - ONE_DAY_MS > yieldEstimate.lastReportedAt) {
+          // TODO: enable once its not busted
+          // sendPlainTextToDiscord(
+          //   `${chain.network} ${vault.name} (${vault.protocol}, ${vault.version}, ${
+          //     vault.state ?? VaultState.Open
+          //   }) failed to harvest!`,
+          // );
+          yieldEstimate.lastReportedAt = now;
+        }
+      }
+
+      const pendingYield = await sdk.vaults.getPendingYield(vault.address);
+      yieldEstimate.yieldTokens = await Promise.all(pendingYield.tokenRewards.map(convert));
+    }
+
+    if (shouldCheckGraph) {
+      yieldEstimate.lastHarvestedAt = await loadGraphTimestamp(sdk, vault);
+    }
+
+    // a harvest happened since we last measured, all previous data is now invalid
+    if (yieldEstimate.lastHarvestedAt > yieldEstimate.lastMeasuredAt) {
+      yieldEstimate.previousHarvestTokens = [];
+      yieldEstimate.previousYieldTokens = [];
+    }
+
+    applyProtocolFees(chain, vault, yieldEstimate);
+    yieldEstimate.duration = now - yieldEstimate.lastMeasuredAt;
+    yieldEstimate.lastMeasuredAt = now;
+
+    return mapper.put(Object.assign(new YieldEstimate(), yieldEstimate));
+  } catch (err) {
+    const message = `Failed to estimate yield for ${vault.name}`;
+    throw new UnprocessableEntity(message, err);
+  }
+}
+
 export async function refreshYieldEstimates() {
   const chains = getSupportedChains();
   const now = Date.now();
 
   for (const chain of chains) {
-    const convert = async (t: TokenBalance) => toTokenValue(chain, t);
-    const sdk = await chain.getSdk();
-    const mapper = getDataMapper();
     const vaults = await chain.vaults.all();
 
     for (const vault of vaults) {
-      if (vault.state === VaultState.Discontinued || vault.protocol !== Protocol.Aura) {
+      if (vault.state === VaultState.Discontinued) {
         continue;
       }
 
-      const existingHarvest = await queryYieldEstimate(vault);
-      const harvestData: YieldEstimate = {
-        vault: vault.address,
-        yieldTokens: [],
-        harvestTokens: [],
-        lastHarvestedAt: 0,
-        previousYieldTokens: existingHarvest.yieldTokens,
-        previousHarvestTokens: existingHarvest.harvestTokens,
-        lastMeasuredAt: existingHarvest.lastMeasuredAt ?? 0,
-        duration: existingHarvest.duration ?? Number.MAX_SAFE_INTEGER,
-        lastReportedAt: existingHarvest.lastReportedAt ?? 0,
-      };
-
-      let shouldCheckGraph = true;
-
-      if (vault.version === VaultVersion.v1_5) {
-        try {
-          const pendingHarvest = await sdk.vaults.getPendingHarvest(vault.address);
-
-          harvestData.harvestTokens = await Promise.all(pendingHarvest.tokenRewards.map(convert));
-
-          harvestData.lastHarvestedAt = pendingHarvest.lastHarvestedAt * 1000;
-          shouldCheckGraph = false;
-        } catch {
-          if (now - ONE_DAY_MS > harvestData.lastReportedAt) {
-            // TODO: enable once its not busted
-            // sendPlainTextToDiscord(
-            //   `${chain.network} ${vault.name} (${vault.protocol}, ${vault.version}, ${
-            //     vault.state ?? VaultState.Open
-            //   }) failed to harvest!`,
-            // );
-            harvestData.lastReportedAt = now;
-          }
-        }
-
-        const pendingYield = await sdk.vaults.getPendingYield(vault.address);
-        harvestData.yieldTokens = await Promise.all(pendingYield.tokenRewards.map(convert));
-      }
-
-      if (shouldCheckGraph) {
-        harvestData.lastHarvestedAt = await loadGraphTimestamp(sdk, vault);
-      }
-
-      // a harvest happened since we last measured, all previous data is now invalid
-      if (harvestData.lastHarvestedAt > harvestData.lastMeasuredAt) {
-        harvestData.previousHarvestTokens = [];
-        harvestData.previousYieldTokens = [];
-      }
-
-      const cachedVault = await getCachedVault(chain, vault);
-      const {
-        strategy: { performanceFee },
-      } = cachedVault;
-      // max fee bps is 10_000, this scales values by the remainder after fees
-      const feeMultiplier = 1 - performanceFee / 10_000;
-      const feeScalingFunction = (t: CachedTokenBalance) => {
-        t.balance *= feeMultiplier;
-        t.value *= feeMultiplier;
-      };
-      harvestData.harvestTokens.forEach(feeScalingFunction);
-      harvestData.yieldTokens.forEach(feeScalingFunction);
-
-      harvestData.harvestTokens.forEach((t) => {
-        if (t.address === vault.depositToken) {
-          t.name = VAULT_SOURCE;
-        }
-      });
-      harvestData.duration = now - harvestData.lastMeasuredAt;
-      harvestData.lastMeasuredAt = now;
-
-      const harvestDifference = calculateBalanceDifference(
-        harvestData.previousHarvestTokens,
-        harvestData.harvestTokens,
-      );
-      const hasNegatives = harvestDifference.some((b) => b.balance < 0);
-
-      // if the difference incur negative values due to slippage or otherwise, force a comparison against the full harvest
-      if (hasNegatives) {
-        harvestData.previousHarvestTokens = [];
-      }
-
-      console.log(`${vault.name} Yield Report`);
-      console.log('Current Yield Tokens');
-      console.table(harvestData.yieldTokens.map(toTableRow));
-      console.log('Current Harvest Tokens');
-      console.table(harvestData.harvestTokens.map(toTableRow));
-      console.log('Previous Yield Tokens');
-      console.table(harvestData.previousYieldTokens.map(toTableRow));
-      console.log('Previous Harvest Tokens');
-      console.table(harvestData.previousHarvestTokens.map(toTableRow));
-
       try {
-        await mapper.put(Object.assign(new YieldEstimate(), harvestData));
+        // TODO: utilize yield estimate to subsequently calculate the projection
+        // save a persisted copy of a validated projection
+        await captureVaultReports(chain, vault, now);
       } catch (err) {
-        console.error({ err, vault });
+        console.error(err);
       }
     }
   }
