02140aaa446f3f07e11107acb3e1c4d53a311920
jintao <hellojintao@protonmail.com>
2022-09-01 17:14:04 -0400

    refactor: force one round per run

--- src/indexers/vault-harvests-indexer.ts
+++ src/indexers/vault-harvests-indexer.ts
@@ -1,9 +1,7 @@
 import { getDataMapper, getVaultEntityId } from '../aws/dynamodb.utils';
-import { HARVEST_SCAN_START_BLOCK } from '../aws/models/vault-definition.model';
 import { VaultYieldEvent } from '../aws/models/vault-yield-event.model';
 import { getSupportedChains } from '../chains/chains.utils';
 import { HARVEST_SCAN_BLOCK_INCREMENT, loadYieldEvents } from '../vaults/harvests.utils';
-import { YieldEvent } from '../vaults/interfaces/yield-event';
 
 export async function updateVaultHarvests() {
   const chains = getSupportedChains();
@@ -16,34 +14,17 @@ export async function updateVaultHarvests() {
 
     for (const vault of vaults) {
       try {
-        // temporary fallback for handling non updated last harvest indexed blocks
-        let lastHarvestBlock = vault.lastHarvestIndexedBlock ?? HARVEST_SCAN_START_BLOCK;
-        let yieldEvents: YieldEvent[] = [];
+        const { name, lastHarvestIndexedBlock } = vault;
 
-        console.log(`[${vault.name}]: Last Indexed Block ${lastHarvestBlock}`);
-        while (true) {
-          yieldEvents = await loadYieldEvents(chain, vault, lastHarvestBlock);
-          console.log(`[${vault.name}]: Discovered ${yieldEvents.length} yield events`);
+        console.log(`[${name}]: Last Indexed Block ${lastHarvestIndexedBlock}`);
+        const yieldEvents = await loadYieldEvents(chain, vault, lastHarvestIndexedBlock);
+        console.log(`[${name}]: Discovered ${yieldEvents.length} yield events`);
 
-          // found some events, let's check them out
-          if (yieldEvents.length > 0) {
-            break;
-          } else {
-            lastHarvestBlock += HARVEST_SCAN_BLOCK_INCREMENT;
-            console.log(
-              `[${vault.name}]: Increment block scan to ${lastHarvestBlock} - ${
-                lastHarvestBlock + HARVEST_SCAN_BLOCK_INCREMENT
-              }`,
-            );
-          }
-
-          // we are checking blocks that do not exist yet, and found no sources - we are up to date!
-          if (lastHarvestBlock >= currentBlock) {
-            break;
-          }
+        if (lastHarvestIndexedBlock + HARVEST_SCAN_BLOCK_INCREMENT > currentBlock) {
+          vault.lastHarvestIndexedBlock = currentBlock;
+        } else {
+          vault.lastHarvestIndexedBlock += HARVEST_SCAN_BLOCK_INCREMENT;
         }
-
-        vault.lastHarvestIndexedBlock = lastHarvestBlock;
 
         if (yieldEvents.length === 0) {
           console.log(`${vault.name} is up to date.`);
