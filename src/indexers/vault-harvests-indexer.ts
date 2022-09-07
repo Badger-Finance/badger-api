f9049d1fe81cefa3f8c344f7e43c2b80d51b336b
jintao <hellojintao@protonmail.com>
2022-09-07 12:30:38 -0500

    fix: remove skip + freq change

--- src/indexers/vault-harvests-indexer.ts
+++ src/indexers/vault-harvests-indexer.ts
@@ -1,7 +1,6 @@
 import { getDataMapper, getVaultEntityId } from '../aws/dynamodb.utils';
 import { VaultYieldEvent } from '../aws/models/vault-yield-event.model';
 import { getSupportedChains } from '../chains/chains.utils';
-import { TOKENS } from '../config/tokens.config';
 import { HARVEST_SCAN_BLOCK_INCREMENT, loadYieldEvents } from '../vaults/harvests.utils';
 
 export async function updateVaultHarvests() {
@@ -14,9 +13,6 @@ export async function updateVaultHarvests() {
     const vaults = await chain.vaults.all();
 
     for (const vault of vaults) {
-      if (vault.address !== TOKENS.BVECVX) {
-        continue;
-      }
       try {
         const { name, lastHarvestIndexedBlock } = vault;
 
