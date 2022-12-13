367f9075f7356bfe7094e61b5421918ccb5a5b2d
namelsking <functionsilence@gmail.com>
2022-12-13 21:06:43 +0200

    chore: use inmemory cache for contracts abi fetch from scans

--- src/utils/scans.utils.ts
+++ src/utils/scans.utils.ts
@@ -1,13 +1,27 @@
 import axios from 'axios';
+import { RawAbiDefinition } from 'typechain/dist/parser/abiParser';
 
 import { sleep } from './process.utils';
+
+const abiCacheMap: {
+  [key: string]: RawAbiDefinition[];
+} = {};
 
 export function formScanApiUrl(explorerUrl: string): string {
   const expUrlObj = new URL(explorerUrl);
   return `${expUrlObj.protocol}//api.${expUrlObj.host}/api`;
 }
 
-export async function getContractAbi(address: string, scanApiUrl: string, withThreshold = true) {
+export async function getContractAbi(
+  address: string,
+  scanApiUrl: string,
+  withThreshold = true,
+  useCache = true,
+): Promise<RawAbiDefinition[] | null> {
+  if (useCache && abiCacheMap[address]) {
+    return abiCacheMap[address];
+  }
+
   // if we don't use private keys for scans they can block us, so it's better to use sleep
   if (withThreshold) await sleep(500);
 
@@ -24,5 +38,11 @@ export async function getContractAbi(address: string, scanApiUrl: string, withTh
     return null;
   }
 
-  return JSON.parse(scanRespJson.result);
+  const contractAbi = JSON.parse(scanRespJson.result);
+
+  if (useCache) {
+    abiCacheMap[address] = contractAbi;
+  }
+
+  return contractAbi;
 }
